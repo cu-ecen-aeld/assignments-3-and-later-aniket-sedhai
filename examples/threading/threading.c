980f9438108b0e797b4a3940f222b8d54c49d2b8
aniket-sedhai <aniketsedhai@gmail.com>
2025-03-24 20:50:02 -0400

    completed threading.c and threading.h

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,40 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int status;
+    int wait_to_obtain_us = thread_func_args->wait_to_obtain_ms * 1000;
+    int wait_to_release_us = thread_func_args->wait_to_release_ms * 1000;
+    
+    status = usleep(wait_to_obtain_us);
+    if (status != 0)
+    {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    
+    status = pthread_mutex_lock(thread_func_args->mutex_ptr);
+    if (status != 0)
+    {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    
+    status = usleep(wait_to_release_us);
+    if (status != 0)
+    {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    } 
+    
+    status = pthread_mutex_unlock(thread_func_args->mutex_ptr);
+    if (status != 0)
+    {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+  
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +61,23 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* thread_param = (struct thread_data*) malloc(sizeof(struct thread_data));
+    if (thread_param == NULL)
+    {
+        return false;
+    }
+    
+    thread_param->mutex_ptr = mutex;
+    thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_param->wait_to_release_ms = wait_to_release_ms;
+    
+    int status = pthread_create(thread, NULL, threadfunc, thread_param);
+    if (status != 0)
+    {
+        free(thread_param);
+        return false;
+    }
+    
+    return true;
 }
 
